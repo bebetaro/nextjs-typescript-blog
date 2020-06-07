2b4c67ee1ab34ad8b0df2581dcf7201fc7b3fcda
bebetaro <takumi.kurokawa.dev@gmail.com>
2020-06-07 17:51:13 +0900

    update OGP path

--- src/components/layout/constants.ts
+++ src/components/layout/constants.ts
@@ -1,5 +1,3 @@
-import path from 'path'
-
 export const NAME = 'Bebetaro'
 export const TITLE = 'Bebetaro\'s learning blog'
-export const OGP_IMAGE = '/images/bebe.jpg'
+export const OGP_IMAGE ="https://nextjs-typescript-blog.now.sh/images/bebe.jpg";
